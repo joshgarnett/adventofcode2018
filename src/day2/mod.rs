9459ba4caa2e6111318f88ef6d6d8f4c3ad7623d
Joshua Garnett <josh.garnett@gmail.com>
2018-12-02 10:00:45 -0500

    Day 2 solve

---
+++ src/day2/mod.rs
@@ -0,0 +1,82 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn part1(filename: &str) -> i32 {
+    let file = File::open(filename).expect("file not found");
+
+    let mut two_leters = 0;
+    let mut three_letters = 0;
+    let mut letters: HashMap<char, i32> = HashMap::new();
+    for line in BufReader::new(file).lines() {
+        for c in line.unwrap().chars() {
+            let count = letters.entry(c).or_insert(0);
+            *count += 1;
+        }
+
+        let mut found_two = false;
+        let mut found_three = false;
+        for val in letters.values() {
+            if *val == 2 {
+                found_two = true;
+            } else if *val == 3 {
+                found_three = true;
+            }
+        }
+
+        if found_two {
+            two_leters += 1;
+        }
+        if found_three {
+            three_letters += 1;
+        }
+
+        letters.clear();
+    }
+    two_leters * three_letters
+}
+
+pub fn part2(filename: &str) -> String {
+    // parse all lines into an array of ints
+    let file = File::open(filename).expect("file not found");
+    let mut boxes: Vec<Vec<char>> = Vec::new();
+    for line in BufReader::new(file).lines() {
+        let mut box_chars: Vec<char> = Vec::new();
+        for c in line.unwrap().chars() {
+            box_chars.push(c);
+        }
+        boxes.push(box_chars);
+    }
+
+    // search for boxes that are the closest match (same letters in same position)
+    let box_count = boxes.len();
+    let char_count = boxes[0].len();
+    let mut box_1 = 0;
+    let mut box_2 = 0;
+    let mut max_diff = std::i32::MAX;
+
+    for index_1 in 0..(box_count - 1) {
+        for index_2 in (index_1 + 1)..box_count {
+            let mut diff = 0;
+            for i in 0..char_count {
+                if boxes[index_1][i] != boxes[index_2][i] {
+                    diff += 1;
+                }
+            }
+            if diff < max_diff {
+                box_1 = index_1;
+                box_2 = index_2;
+                max_diff = diff;
+            }
+        }
+    }
+
+    let mut answer: Vec<char> = Vec::new();
+    for i in 0..char_count {
+        if boxes[box_1][i] == boxes[box_2][i] {
+            answer.push(boxes[box_1][i]);
+        }
+    }
+
+    answer.into_iter().collect()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate advent;
 
 use advent::day1;
+use advent::day2;
 use std::time::Instant;
 
 fn time<T>(name: &str, filename: &str, f: &Fn(&str) -> T) where T: std::fmt::Debug {
@@ -12,4 +13,6 @@ fn time<T>(name: &str, filename: &str, f: &Fn(&str) -> T) where T: std::fmt::Deb
 fn main() {
     time("Day1 Part1", "data/day1-input.txt", &day1::part1);
     time("Day1 Part2", "data/day1-input.txt", &day1::part2);
+    time("Day2 Part1", "data/day2-input.txt", &day2::part1);
+    time("Day2 Part2", "data/day2-input.txt", &day2::part2);
 }
